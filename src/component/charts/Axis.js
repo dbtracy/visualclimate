c2399c7ee322907cf0356b8e2637e176a92a5eba
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-08-29 15:37:05 -0400

    FIX(chartapp): deleted ChartApp component

--- src/component/charts/Axis.js
+++ src/component/charts/Axis.js
@@ -12,20 +12,20 @@ export default class Axis extends Component {
     this.renderAxis();
   }
   renderAxis() {
-    const axisType = `axis${this.props.orient}`;
+    // const axisType = `axis${this.props.orient}`;
     const axisl = d3Axis
       .axisLeft()
       .scale(this.props.scale)
       // .tickSize(-this.props.tickSize)
       .tickPadding([3])
       .ticks([4]);
-      // .text()
-    const axisb = d3Axis
-      .axisBottom()
-      .scale(this.props.scale)
-      .ticks([3]);
+    // .text()
+    // const axisb = d3Axis
+    //   .axisBottom()
+    //   .scale(this.props.scale)
+    //   .ticks([3]);
     d3Select(this.axisElement).call(axisl);
-      // d3Select(this.axisElement).call(axisb)
+    // d3Select(this.axisElement).call(axisb)
   }
 
   render() {
