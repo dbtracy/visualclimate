1c215f8df6cb07e21efbc07d3abc7e76e9533270
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-08-30 11:24:52 -0400

    FIX(PDSI script): removed console logs

--- script-PDSI.js
+++ script-PDSI.js
@@ -6,15 +6,12 @@ const yearAverage = months => {
     let pdsi = month["PDSI"];
     const year = String(month.YearMonth).slice(0, 4);
     if (typeof pdsi !== "number") {
-      // console.log("WHAT IS THIS", pdsi);
       if (pdsi.includes("-.")) {
         pdsi = pdsi.split("");
         pdsi.shift();
         pdsi.unshift("0");
         pdsi = pdsi.join("");
         pdsi = pdsi * -1;
-        // console.log("this is pdsi", pdsi, "typeof", typeof pdsi);
-        // console.log("TRANSFORMED", pdsi, "TYPEOF", typeof pdsi);
       } else if (pdsi[0] === ".") {
         parseFloat(pdsi);
       }
@@ -22,7 +19,6 @@ const yearAverage = months => {
     if (!newObj[year]) newObj[year] = parseFloat(pdsi);
     else newObj[year] = newObj[year] + parseFloat(pdsi);
   });
-  // console.log(newObj)
   for (let year in newObj) {
     newObj[year] = (newObj[year] / 12).toFixed(2);
   }
