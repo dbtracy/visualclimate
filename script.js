717dd91206f069c8361542c7bb9907878679acee
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-08-30 11:25:31 -0400

    FIX(Temp script): removed console logs

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const yearAverage = months => {
     let temp = month["TAVG"];
     const year = String(month.YearMonth).slice(0, 4);
     if (typeof temp !== "number") {
-      // console.log("WHAT IS THIS", temp);
       if (temp.includes("-.")) {
         temp = temp.split("");
         temp.shift();
@@ -14,7 +13,6 @@ const yearAverage = months => {
         temp = temp.join("");
         temp = temp * -1;
         console.log("this is temp", temp, "typeof", typeof temp);
-        // console.log("TRANSFORMED", temp, "TYPEOF", typeof temp);
       } else if (temp[0] === ".") {
         parseFloat(temp);
       }
@@ -22,7 +20,6 @@ const yearAverage = months => {
     if (!newObj[year]) newObj[year] = parseFloat(temp);
     else newObj[year] = newObj[year] + parseFloat(temp);
   });
-  // console.log(newObj)
   for (let year in newObj) {
     newObj[year] = (newObj[year] / 12).toFixed(2);
   }
