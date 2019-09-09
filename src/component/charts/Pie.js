ba2875836a3eca537653d3d35580cba376659612
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-09-09 12:11:31 -0400

    FIX(various imports, style objects): deleted unnecessary Component imports, changed style objects on a few componenet

--- src/component/charts/Pie.js
+++ src/component/charts/Pie.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import *  as d3 from 'd3';
 import Slice from './Slice'
-import gases from './gases';
-import ToolTipPie from './Tooltip';
 class Pie extends React.Component {
-    constructor(props) {
-      super(props);
-      this.colorScale = ['#4fc3f7','#cfd8dc','#80cbc4','#7986cb'];
-      this.renderSlice = this.renderSlice.bind(this);
-      this.labels = this.props.data.map(function(v){
-        return v.couse
-      })
-    }
-    renderSlice(value, i) {
-        let {innerRadius, outerRadius, cornerRadius, padAngle} = this.props;
-        return (
-          <Slice key={i}
-            innerRadius={innerRadius}
-            outerRadius={outerRadius}
-            cornerRadius={cornerRadius}
-            padAngle={padAngle}
-            value={value}
-            label={this.labels[i]}
-            fill={this.colorScale[i]} />
-          );
-    }
-    render() {
-      let {x, y, data} = this.props;
-      let pie = d3.pie();
-      let data_values=data.map(function(v){
-        return v.value
-      })
-      return (
-        <g transform={`translate(${x}, ${y})`}>
-          {/* Render a slice for each data point */}
-          {pie(data_values).map(this.renderSlice)}
-        </g>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.colorScale = ['#4fc3f7', '#cfd8dc', '#80cbc4', '#7986cb'];
+    this.renderSlice = this.renderSlice.bind(this);
+    this.labels = this.props.data.map(function (v) {
+      return v.couse
+    })
   }
+  renderSlice(value, i) {
+    let { innerRadius, outerRadius, cornerRadius, padAngle } = this.props;
+    return (
+      <Slice key={i}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        cornerRadius={cornerRadius}
+        padAngle={padAngle}
+        value={value}
+        label={this.labels[i]}
+        fill={this.colorScale[i]} />
+    );
+  }
+  render() {
+    let { x, y, data } = this.props;
+    let pie = d3.pie();
+    let data_values = data.map(function (v) {
+      return v.value
+    })
+    return (
+      <g transform={`translate(${x}, ${y})`}>
+        {/* Render a slice for each data point */}
+        {pie(data_values).map(this.renderSlice)}
+      </g>
+    );
+  }
+}
 export default Pie
--- src/component/charts/ToolTipLine.js
+++ src/component/charts/ToolTipLine.js
@@ -2,21 +2,23 @@ import React from 'react'
 import './Tooltip.css'
 import Paper from '@material-ui/core/Paper';
 
-export default ({hoveredBar, scales}) => {
-    const { xScale, yScale } = scales
-    const styles = {
-      left: `${xScale(hoveredBar.date)}px`,
-      top: `${yScale(hoveredBar.y)}px`
-    }
-    return (
-      <div >
-        <Paper  className="Tooltip" style={styles} >
-          <tr ><b>{hoveredBar.date}</b></tr>
-          <td >Temp: {hoveredBar.y}</td>
-        </Paper>
-        <rect width="130" height="15" style="fill: rgb(238, 238, 238); opacity: 0.8;"></rect>
-        <text class="featText" dx="20" dy="30">408</text>
-        <circle y={yScale(hoveredBar.y)} r="6" style="fill: rgb(255, 255, 255); stroke: rgb(0, 177, 81);"></circle>
-      </div>
-    )
+export default ({ hoveredBar, scales }) => {
+  const { xScale, yScale } = scales
+  const styles = {
+    left: `${xScale(hoveredBar.date)}px`,
+    top: `${yScale(hoveredBar.y)}px`
   }
+  return (
+    <div >
+      <Paper className="Tooltip" style={styles} >
+        <tr ><b>{hoveredBar.date}</b></tr>
+        <td >Temp: {hoveredBar.y}</td>
+      </Paper>
+      <rect width="130" height="15" style={{
+        fill: 'rgb(238, 238, 238)', opacity: '0.8'
+      }}></rect>
+      <text class="featText" dx="20" dy="30" > 408</text>
+      <circle y={yScale(hoveredBar.y)} r="6" style={{ fill: 'rgb(255, 255, 255)', stroke: 'rgb(0, 177, 81)' }}></circle>
+    </div>
+  )
+}
